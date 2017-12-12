5cb660f678eb647650644eefd5b2c2f984a5b3ea
David Eliason <davethemaker@gmail.com>
2017-12-11 17:38:53 -0800

    successfully call getQuotes function with the result of the passed state being populated with the JSON data objects in Firebase DB

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Row,Col,Jumbotron} from 'react-bootstrap';
+
 
 class App extends Component {
+
+	 componentDidMount() {
+   		 this.props.getQuotes(); // FB DB object set to redux
+  	 }
+
   render() {
     return (
       <div className="App">
-         <p>hello</p>
+         <p>hello world</p>
       </div>
     );
   }
