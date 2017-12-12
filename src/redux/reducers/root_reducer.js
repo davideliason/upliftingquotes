9dd6cd967a07fcc8b4c3e4d077b4e4f031d8cb08
David Eliason <davethemaker@gmail.com>
2017-12-11 18:01:13 -0800

    add filter to combinedReducer and it shows in redux state successfully

--- src/redux/reducers/root_reducer.js
+++ src/redux/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import quotesReducer from './quotes_reducer.js';
+import filterReducer from './filter_reducer.js';
 
 const rootReducer = combineReducers({
-	quotes: quotesReducer
+	quotes: quotesReducer,
+	filter: filterReducer
 });
 
 export default rootReducer;
