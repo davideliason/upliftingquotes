70d43eaa06ed8694a728987ef32b0f0fc37a2004
David Eliason <davethemaker@gmail.com>
2017-12-12 08:49:27 -0800

    API get calls for JSON data objects of pictures and random quotes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,16 @@ import './App.css';
 import {Row,Col,Jumbotron} from 'react-bootstrap';
 import AppButtonOne from './AppButtonOne.js';
 import Background from './Background.js';
+import BackgroundPic from './sunshine-and-clouds-background.jpg';
+var sectionStyle = {
+  width: "100%",
+  height: "100%",
+  backgroundImage: "url(" + BackgroundPic + ")",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover"
+};
+
 const colorStyle = {
 	color: 'green'
 }
@@ -18,7 +28,7 @@ class App extends Component {
 	const {quotes} = this.props.quotes;
 
     return (
-      <div className="App">
+      <div style = { sectionStyle } >
         <Jumbotron>
           <h3>Inspirational Quotes</h3>
         	<Row>
--- src/components/App/Background.js
+++ src/components/App/Background.js
@@ -11,14 +11,15 @@ class Background extends Component {
     super();
     this.state = {
       pictures: [],
+      quotes: []
     };
   }
 
 	 componentDidMount() {
       fetch('https://randomuser.me/api/?results=500')
-      .then(results => {
-        return results.json();
-      }).then(data => {
+       .then(results => {
+         return results.json();
+       }).then(data => {
         let pictures = data.results.map((pic) => {
           return (
               <div key={pic.results}>
@@ -26,9 +27,22 @@ class Background extends Component {
               </div>
             )
         })
-        this.setState({pictures: pictures});
-        console.log("state", this.state.pictures);
-      })
+          this.setState({pictures: pictures});
+       })
+
+       fetch('https://random-quote-generator.herokuapp.com/api/quotes/')
+       .then(results => {
+         return results.json();
+       }).then(data => {
+        let quotes = data.map((quote) => {
+          return (
+              <div key={quote.results}>
+               {quote.author}
+              </div>
+            )
+        })
+          this.setState({quotes: quotes});
+       })
   	 }
 
   render() {
@@ -37,6 +51,7 @@ class Background extends Component {
       <div >
         <p>Background</p>
         <div>
+          {this.state.quotes}
           {this.state.pictures}
         </div>
       </div>
