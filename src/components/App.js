8af90f61f0001891889a04124844b3e833da6c79
David Eliason <davethemaker@gmail.com>
2017-12-11 17:44:23 -0800

    add links to personal VCS repos

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-         <p>hello world</p>
+        <Jumbotron>
+          <h3>Inspirational Quotes</h3>
+        	<Row>
+             	<Col xs={2}></Col>
+             	<Col xs={2}>
+                	<a href="https://github.com/davideliason/favorite-books-react-redux-firebase">Repo</a>
+            	 </Col>
+
+             	<Col xs={2}></Col>
+                <Col xs={2}>
+              	  <a href="https://github.com/davideliason/davideliason.github.io">Github Portfolio</a>
+             	 </Col>
+             	<Col xs={2}></Col>
+           </Row>
+        </Jumbotron>
       </div>
     );
   }
